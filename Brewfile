7758c8cf0c7906761d3c9ddc6b86f1e64f07ec11
Nils Hendriks <694301+nilshendriks@users.noreply.github.com>
2025-02-03 10:37:39 +0100

    added Brewfile

---
+++ Brewfile
@@ -0,0 +1,12 @@
+# Taps
+tap "homebrew/bundle"
+
+# Brews (Packages)
+brew "bat"
+brew "httpie"
+# brew "exa"
+
+# Casks (Applications)
+cask "firefox@developer-edition"
+cask "visual-studio-code"
+cask "zed"
---
+++ setup_homebrew.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env zsh
+
+echo "\n<<< Starting Homebrew Setup >>>\n"
+
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+
+# brew install httpie
+# brew install bat
+
+# Casks 
+# brew install --no-quarantine google-chrome
+# brew install --no-quarantine firefox@developer-edition
+# brew install --no-quarantine visual-studio-code
+# brew install --no-quarantine zed
+
+# new to install yet 
+# brew install ghostty
+
+# later remove current installs and re-install with homebrew 
+# brew install 1password
+# brew install alfred
+# brew install wezterm
+# brew install 
+
+brew bundle --verbose 
