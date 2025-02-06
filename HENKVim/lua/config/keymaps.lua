de6338a81d4a66c4f2ea1dce694ffd3fae23af58
Nils Hendriks <694301+nilshendriks@users.noreply.github.com>
2025-02-06 13:05:49 +0100

    added treesitters for html, twig

--- HENKVim/lua/config/keymaps.lua
+++ HENKVim/lua/config/keymaps.lua
@@ -1,3 +1,19 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+
+local keymap = vim.keymap
+local opts = { noremap = true, silent = true }
+
+-- Set keybinding for opening Snacks dashboard
+-- keymap.set("n", "<leader>db", ":lua require('snacks').dashboard.open()<CR>", opts)
+keymap.set(
+  "n",
+  "<leader>db",
+  ":lua Snacks.dashboard.open()<CR>",
+  vim.tbl_extend("force", opts, { desc = "Open Dashboard" })
+)
+
+-- thanks theprimeagen
+vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
+vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
