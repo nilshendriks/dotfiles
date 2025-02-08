7c7382de543c39b9c35e0fde5ebd568366111c68
Nils Hendriks <694301+nilshendriks@users.noreply.github.com>
2025-02-08 14:46:38 +0100

    added zshenv, exist function

--- setup_homebrew.sh
+++ setup_homebrew.sh
@@ -2,31 +2,18 @@
 
 echo "\n<<< Starting Homebrew Setup >>>\n"
 
-# /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-
-# brew install httpie
-# brew install bat
-
-# Casks
-# brew install --no-quarantine google-chrome
-# brew install --no-quarantine firefox@developer-edition
-# brew install --no-quarantine visual-studio-code
-# brew install --no-quarantine zed
-
-# new to install yet
-# brew install ghostty
-
-# later remove current installs and re-install with homebrew
-# brew install 1password
-# brew install alfred
-# brew install wezterm
-# brew install
-
-if ! command -v brew &>/dev/null; then
-  echo "Homebrew not found. Installing..."
-  /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+if exists brew; then
+  echo "brew exists, skipping install"
 else
-  echo "Homebrew is already installed. Skipping installation."
+  echo "brew doesn't exist, continuing with install"
+  /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 fi
+
+# if ! command -v brew &>/dev/null; then
+#   echo "Homebrew not found. Installing..."
+#   /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+# else
+#   echo "Homebrew is already installed. Skipping installation."
+# fi
 
 brew bundle --verbose
