6080f7e2cdcb31d2b3bd495ae494f3a13312ffea
Nils Hendriks <694301+nilshendriks@users.noreply.github.com>
2025-02-08 19:15:27 +0100

    added setup script for node and added to yaml

---
+++ setup_node.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env zsh
+
+echo "\n<<< Starting Node Setup >>>\n"
+
+# Node versions are managed with n, which is in the brewfile.
+# See zshrc for N_Prefix variable and addition to PATH.
+
+if exists node; then
+  echo "Node & NPM already installed"
+else
+  echo "Installing Node & NPM with n..."
+  n latest
+fi
