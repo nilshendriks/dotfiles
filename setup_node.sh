ebacd00df768224ca5653198f3c9ee41cf0ff598
Nils Hendriks <694301+nilshendriks@users.noreply.github.com>
2025-02-08 19:39:10 +0100

    added npm json-server and http-server

--- setup_node.sh
+++ setup_node.sh
@@ -11,3 +11,10 @@ else
   echo "Installing Node & NPM with n..."
   n latest
 fi
+
+# Install Global NPM Packages - if needed
+npm install --global json-server
+npm install --global http-server
+
+echo "Global NPM Packages Installed:"
+npm list --global --depth=0
