e564d797b2c6b350196057d939ee67cdd889ac21
Nils Hendriks <694301+nilshendriks@users.noreply.github.com>
2025-05-20 15:57:00 +0200

    removed extras from plugins

--- HENKVim/lua/plugins/extras.lua
+++
@@ -1,9 +0,0 @@
-return {
-  { import = "lazyvim.plugins.extras.coding.mini-surround" },
-  -- { import = "lazyvim.plugins.extras.linting.eslint" },
-  -- { import = "lazyvim.plugins.extras.formatting.prettier" },
-  -- { import = "lazyvim.plugins.extras.formatting.biome" },
-  { import = "lazyvim.plugins.extras.lang.typescript" },
-  { import = "lazyvim.plugins.extras.lang.vue" },
-  -- { import = "lazyvim.plugins.extras.util.mini-hipatterns" },
-}
