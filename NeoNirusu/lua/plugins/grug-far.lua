95dd03efb0dd358567aefc28b3d0c3f5638fcf1f
Nils Hendriks <694301+nilshendriks@users.noreply.github.com>
2025-09-04 15:13:58 +0200

    added grug-far plugin for project find and replace

---
+++ NeoNirusu/lua/plugins/grug-far.lua
@@ -0,0 +1,8 @@
+return {
+    "MagicDuck/grug-far.nvim",
+    opts = {},
+    keys = {
+        { "<leader>sr", "<cmd>GrugFar<cr>", desc = "Search & Replace (grug-far)" },
+        { "<leader>sR", "<cmd>GrugFarWord<cr>", desc = "Search & Replace Word" },
+    },
+}
